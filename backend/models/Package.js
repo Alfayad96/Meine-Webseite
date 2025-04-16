0c1009354a13ad6b1ce1160301e9d45f140f632d
Mohammad Alfayad <MALF197@more2learn.de>
2025-04-16 12:28:01 +0200

    Update .gitignore to ignore .env and node_modules

--- backend/models/Package.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const packageSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-});
-
-module.exports = mongoose.model('Package', packageSchema);
